2ccc6d24f24a95d3b61365f2348e9b5aa5b40364
Steven_Villegas <syivenvillegas0@gmail.com>
2024-08-05 17:38:13 -0500

    video numero 10

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -16,6 +16,8 @@ export const TasksProvider = ({ children }) => {
 
   const [tasks,setTasks] = useState(tsk)
   const [filteredTasks,setFilteredTasks] = useState(tsk)
+  const [pendingTasks, setPendingTasks] = useState(0);
+  const [doneTasks, setDoneTasks] = useState(0);
 
   const updateTaskStatus = ( taskId , newStatus) => {
     setTasks(prevTasks => prevTasks.map(task => task.id === taskId ? {...task, status: newStatus} : task)
@@ -25,11 +27,22 @@ export const TasksProvider = ({ children }) => {
 
 
   return (
-<taskContext.Provider value={{tasks , setTasks,filteredTasks,setFilteredTasks,updateTaskStatus}}>
-  
+<tasksContext.Provider value={{
+  tasks,
+  setTasks,
+  pendingTasks,
+  setPendingTasks,
+  doneTasks,
+  setDoneTasks,
+  isChecked,
+  setIsChecked,
+  updateTaskStatus,
+  filteredTasks,
+  setFilteredTasks
+}}>
 {children}
 
-</taskContext.Provider>
+</tasksContext.Provider>
 
   )  
 }
--- src/components/Layouts/InfoTasks/InfoTasks.jsx
+++ src/components/Layouts/InfoTasks/InfoTasks.jsx
@@ -1,9 +1,23 @@
-import './infoTasks.css'
+import React, { useContext, useEffect } from 'react';
+import './infoTasks.css';
+
+import { tasksContext } from "../../Context/Context";
 
 export const InfoTasks = () => {
+  const context = useContext(tasksContext);
+
+  useEffect(() => {
+    let pending = context.tasks.filter(tasks => tasks.status === false);
+    let resolve = context.tasks.filter(tasks => tasks.status === true);
+
+    context.setPendingTasks(pending.length);
+    context.setDoneTasks(resolve.length);
+  }, [context.tasks]);
 
   return(
-    <>    
+    <>   
+    <h2 className='info-tasks'>usted tiene<span className='task-pending'>{context.pendingTasks}</span> pendientes y <span className='task-done'>{context.doneTasks}</span>terminadas</h2> 
+    <hr />
     </>
   )
 } 
